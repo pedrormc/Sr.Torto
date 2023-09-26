2be217f17c191573ea60656c7c74e62c893b1903
pedrormc <113632337+pedrormc@users.noreply.github.com>
2023-09-26 19:51:25 -0300

    cadastro,criptografia e autenticação rodando

--- sr.torto/app.react/api/controllers/user.js
+++ sr.torto/app.react/api/controllers/user.js
@@ -1,4 +1,6 @@
 import { db } from "../db.js";
+import bcrypt from "bcrypt";
+const saltRounds = 10;
 
 export const getUsers = (_, res) => {
   const q = "SELECT * FROM users";
@@ -16,7 +18,7 @@ export const addUser = (req, res) => {
     "SELECT * FROM users WHERE email = ?"
 
   const qd =
-    "INSERT INTO users(`nickname`, `email`, `senha`) VALUES(?)";
+    "INSERT INTO users(`nickname`, `email`, `senha`) VALUES(?,?,?)";
 
   const values = [
     req.body.nickname,
@@ -24,13 +26,33 @@ export const addUser = (req, res) => {
     req.body.senha,
     
   ];
+  db.query("SELECT * FROM users WHERE email = ?", values[1], (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    if (result.length == 0) {
+      bcrypt.hash(values[2], saltRounds, (err, hash) => {
 
-  db.query(qd, [values], (err) => {
-    if (err) return res.json(err);
-
-    return res.status(200).json("Usuário criado com sucesso.");
+        db.query(qd, [values[0],values[1], hash], (err) => {
+          if (err) return res.json(err);
+      
+          return res.status(200).json("Usuário criado com sucesso.");
+        });
+      });
+    } else {
+      res.send({ msg: "Email já cadastrado" });
+    }
   });
 };
+
+//   db.query(qd, [values], (err) => {
+//     if (err) return res.json(err);
+
+//     return res.status(200).json("Usuário criado com sucesso.");
+//   });
+
+
+
 
 export const updateUser = (req, res) => {
   const q =
@@ -51,7 +73,7 @@ export const updateUser = (req, res) => {
 };
 
 export const deleteUser = (req, res) => {
-  const q = "DELETE FROM users WHERE `id` = ?";
+  const q = "DELETE FROM users WHERE `id_player` = ?";
 
   db.query(q, [req.params.id], (err) => {
     if (err) return res.json(err);
