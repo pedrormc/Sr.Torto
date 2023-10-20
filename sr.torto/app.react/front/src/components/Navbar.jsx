d0d4bc368c0109f0e7ea9af42d43849acd1bd10f
pedrormc <113632337+pedrormc@users.noreply.github.com>
2023-10-20 11:04:34 -0300

    css

--- sr.torto/app.react/front/src/components/Navbar.jsx
+++ sr.torto/app.react/front/src/components/Navbar.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Se você estiver usando React Router para navegação
 import styled from "styled-components"
+import GlobalStyle from "../styles/Navbar";
 
 
 
@@ -9,53 +10,30 @@ import styled from "styled-components"
 
 
 const Bloco = styled.div`
-  width: 1900px;
-  height: 15%;
-  margin-top: 20px;
-  display: flex;
- border : 2px solid black;
+
 
   
 `;
-const Logo = styled.div`
-  
+const Logo = styled.div``;
 
-  
-`;
+const Nav = styled.nav``;
 
-const Nav = styled.nav`
-  list-style-type: none;
-  width: 100%;
-  height: 100%;
-  text-align: right;
-  display:flex;
-  
-`;
-const Ul  = styled.ul`
-list-style-type: none;
-margin:0;
-padding:0;
-overflow: hidden;
-background-color: #ff8c00;
-text-align: center;
-display:flex;
-`
-const Li = styled.li`
-display: block;
-color: rgb(0,0,0);
-text-align: center;
-padding : 18px 30px;
-text-decoration: none;
-`
+const Ul  = styled.ul``
+
+const Li = styled.li``
+
+
 
 
 
 const Navbar = () => {
   return (
+      <>
       
+
     <Bloco>
     <Nav>
-      <Logo><img src="./src/img/papa.jpg" alt="seila" />aaaaLogo</Logo>
+      <Logo><img src="./src/img/logo-torta.png" alt="seila" />aaaaLogo</Logo>
       <Ul>
         <Li><Link to="/home">Home</Link></Li>
         <Li><Link to="/cadastro">Cadastro</Link></Li>
@@ -66,6 +44,9 @@ const Navbar = () => {
       </Ul>
     </Nav>
     </Bloco>
+    
+      <GlobalStyle/>
+      </>
   );
 };
 
--- sr.torto/app.react/front/src/styles/navbar.jsx
+++ sr.torto/app.react/front/src/styles/navbar.jsx
@@ -1,46 +1,22 @@
 import { createGlobalStyle } from "styled-components";
 
-const Navbara = createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
 
   * {
-    margin: 0;
+    margin: 0px;
     padding: 0;
     font-family: 'poppins', sans-serif;
     
   }
   
-  container-principal {
-    font-family: 'verdana'; color:#777;}
-
-body{
-    background-color: #777;
-}
-
-.logo {
-    border: 1px solid black;
-    width: 100%;
-    height: 120px;
-    background-color:#000000;
+  body {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    background-color: #474747;
   }
   
-  img {
-    width: 400px;
-
-    object-fit: fill;
-    position: relative;
-    left: 2px;
-    top: 35px;
-    
-  }
-
-
-
-  nav{
-    list-style-type:none;
-    margin: 0;
-    padding: 0;
-  }
-
   ul {
     list-style-type: none;
     margin: 0;
@@ -49,7 +25,7 @@ body{
     background-color: #ff8c00;
     text-align: center;
   }
-  
+
   li {
     float: left;
   }
@@ -72,57 +48,32 @@ body{
     border-bottom: solid 5px #000000;
 
   }
+  .logo {
+    border: 1px solid black;
+    width: 100%;
+    height: 120px;
+    background-color:#000000;
+  }
+  
+  img {
+    width: 400px;
 
-  
-  #divBusca{
+    object-fit: fill;
+    position: relative;
+    left: 2px;
+    top: 35px;
     
-    position: absolute;
-    z-index: 1;
-    left: 500px;
-    top: 50px;
-  }
-  
-  #txtBusca{
-    float:left;
-    background-color:transparent;
-    padding-left:5px;
-    font-size:18px;
-    border:none;
-    height:32px;
-    width:433px;
-  }
-  
-  #btnBusca{
-    border:none;
-    position:relative;
-    left: 0px;
-    height:32px;
-    border-radius:0 7px 7px 0;
-    color: #000000;
-    width:60px;
-    font-weight:bold;
-    background:#ff8c00;
-    
-    color: #000000;
-    }
-    #bntBusca a:hover {
-      background-color: #111;
-      color: #E0EEEE;
-      border-bottom: #ff8c00;
-  
-    }
-  
-  .droperfil{
-    position: absolute;
-    z-index: 1;
-    left: 87%;
-    top: 50px ;
   }
 
-  .container-fluid{
-    width: 900px;
+  nav{
+    list-style-type:none;
+    margin: 0;
+    padding: 0;
+  }
+  #bloco{
+    background-color: #FFBE27;
   }
 `;
 
-export default Navbara;
+export default GlobalStyle;
 
