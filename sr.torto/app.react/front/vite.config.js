d27fe225e10d1157f09fd610733648c242e81970
pedrormc <113632337+pedrormc@users.noreply.github.com>
2023-10-22 14:51:43 -0300

    resolução de conflitos do vite.config.js

--- sr.torto/app.react/front/vite.config.js
+++ sr.torto/app.react/front/vite.config.js
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import VitePluginImages from 'vite-plugin-vue-images'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePluginImages()
   ],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Se o seu código estiver em um diretório 'src', ajuste conforme necessário
+    },
+  },
+  server: {
+    open: true,
+  },
+  build: {
+    assetsInlineLimit: 0, // Permite a inclusão de imagens diretamente no código
+  },
+});
+
